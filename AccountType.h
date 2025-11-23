aa5e25a77b6153373755e8a023e8a7376ee54d46
Joseph Campbell <jcampbell172@my.bcit.ca>
2025-11-22 21:33:17 -0800

    Adding AccountType.h

---
+++ AccountType.h
@@ -0,0 +1,12 @@
+#pragma once
+
+/**
+ * AccountType enumeration
+ * Defines the different types of accounts supported by the banking system
+ */
+enum class AccountType {
+    Savings,
+    Chequing,
+    TFSA
+
+};
